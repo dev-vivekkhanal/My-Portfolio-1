5289db6e7db8897a0053da037e0c05a6f05b375e
Vivek <dev.vivekkhanal@gmail.com>
2021-12-23 13:50:14 +0530

    dextop site 1

--- resources/js/index.js
+++ resources/js/index.js
@@ -4,47 +4,39 @@ const navLinks = document.querySelectorAll(".nav-links li")
 
 
 
-
 const navSlide = () => {
    
-
-    
-     
-    
     menu.addEventListener('click' , () => {
-
         // Toggle Nav
         nav.classList.toggle('nav-active')
-
         // Animate links
         navLinks.forEach((link, index) => {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 15 + 0.2}s` 
         })
-
         // menu animation
         menu.classList.toggle('toggle')
-    })
 
-    
-
-    
+        
+    })    
 }
 
 
 
-const eachNavLinks = document.querySelectorAll(".nav-links li a")
-     console.log(eachNavLinks) 
-
-     function autoClose(){
-        nav.classList.remove('nav-active')
-        menu.classList.remove('toggle')
+function autoClose(){
+    nav.classList.remove('nav-active')
+    menu.classList.remove('toggle')
 
      }
+
+     
+
+
  
 autoClose()
 navSlide()
 
-
+    
+   
 
 // // // Animate links
 // navLinks.forEach((link, index) => {
